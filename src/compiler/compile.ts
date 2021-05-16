0d084815bc0916c9d3c4576f4e17b947369e87fe
Daniel Sepulveda <danielgsepulvedaa@gmail.com>
2021-05-16 18:03:09 -0500

    feat(functions)!: generate intermediate code for functions and handle compilation memory (#10)

--- src/compiler/compile.ts
+++ src/compiler/compile.ts
@@ -1,21 +1,10 @@
 import ohm from 'ohm-js';
-import * as fs from 'fs';
-import * as path from 'path';
 import semantics from './semantics';
-import * as symbolTable from './symbolTable';
-import { jsonLog, jsonStringify } from '../utils/helpers';
+import { logAll } from './logger';
 
 const compile = (input: ohm.MatchResult) => {
   semantics(input).applySemantics();
-  // jsonLog(symbolTable.internal.stacks.operandStack.toArray());
-  // jsonLog(symbolTable.internal.stacks.operatorStack.toArray());
-  fs.writeFileSync(
-    path.join(__dirname, 'quadruples.out.txt'),
-    jsonStringify(symbolTable.internal.quadrupleArr)
-  );
-  // symbolTable.internal.quadrupleArr.forEach((quad) =>
-  //   console.log(`${quad.count}: ${jsonStringify(quad)}`)
-  // );
+  logAll();
 };
 
 export default compile;
