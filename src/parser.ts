e53bd2cf39fdb26fbddd51f0ad17d72572b6a046
Daniel Sepulveda <danielgsepulvedaa@gmail.com>
2021-06-01 12:51:26 -0500

    chore(parser): add comments

--- src/parser.ts
+++ src/parser.ts
@@ -1,5 +1,14 @@
 import grammar from './grammar';
 
+/**
+ * Parse function receives a string which corresponds to a
+ * Compiscript program, then it validates that program against
+ * the grammar. If it succeeds, it returns a valid ohm result,
+ * otherwise it throws an error.
+ *
+ * @param input `string`
+ * @returns `ohm.MatchResult`
+ */
 const parse = (input: string) => {
   const matchResult = grammar.match(input);
   if (matchResult.succeeded()) {
